f40a14148e703345c8842638f48eabe7509bc842
Samdipro Nainggolan <71158606+samdipro@users.noreply.github.com>
2023-05-18 13:42:22 +0700

    commit1

--- frontend/atttendace-app/src/App.js
+++ frontend/atttendace-app/src/App.js
@@ -1,4 +1,3 @@
-import { Box } from "@chakra-ui/react";
 import "./App.css";
 import DashboardPage from "./pages/Dashboard";
 
--- frontend/atttendace-app/src/pages/Dashboard.jsx
+++ frontend/atttendace-app/src/pages/Dashboard.jsx
@@ -1,25 +1,145 @@
-import { Box, Container, Flex, Button } from "@chakra-ui/react";
+import { Box, Container, Flex, Center } from "@chakra-ui/react";
+import { useState } from "react";
+import Moment from "react-moment";
+import axios from "axios";
 
 export default function DashboardPage() {
+  // const [clickInHas, setClickInHas] = useState(false);
+  const [clockIn, setClockIn] = useState([]);
+  const [isClickIn, setIsClickIn] = useState(false);
+
+  const cin = () => {
+    if (!isClickIn) {
+      pushCin();
+      const timeClicked = new Date();
+      setClockIn([...clockIn, timeClicked]);
+      setIsClickIn(true);
+      // setClickInHas(false);
+    }
+  };
+
+  const [clockOut, setClockOut] = useState([]);
+  const [isClickOut, setIsClickOut] = useState(false);
+
+  const cout = () => {
+    // if (clickInHas) {
+    if (!isClickOut) {
+      const timeClicked = new Date();
+      setClockOut([...clockOut, timeClicked]);
+      setIsClickOut(true);
+    }
+    // }
+  };
+
+  // async function pushCin() {
+  // const []
+  // await axios.get("http:/localhost:2000/").then((req, res)=>{
+
+  //   })
+  // }
+
   return (
     <>
-      <Container size={"sm"}>
-        <Box>
+      <Container
+        maxWidth={"400px"}
+        height={"100vh"}
+        bgColor={"#F1F6F9"}
+        boxShadow={
+          "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px"
+        }
+        fontSize={"xl"}
+      >
+        <Flex flexDir={"column"} gap={"10px"}>
           <Box>
-            <h4>Live Attendance</h4>
-            <Box>CLock</Box>
-            <Box>Date</Box>
+            <Center>
+              <h4>Live Attendance</h4>
+            </Center>
+            <Center>
+              <Box fontSize={"2xl"} fontWeight={"bold"}>
+                <Moment format="LT"></Moment>
+              </Box>
+            </Center>
+            <Center>
+              <Moment format="LL"></Moment>
+            </Center>
           </Box>
-          <Box>
-            <h4>schedule 17 May 2023</h4>
+          <Box
+            boxShadow={
+              "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px"
+            }
+            bgColor={"#212A3E"}
+            color={"#F1F6F9"}
+            padding={"5px"}
+            textAlign={"center"}
+          >
+            <h4>
+              schedule <Moment format="LL"></Moment>
+            </h4>
             <Box>SH20PA</Box>
             <Box>08:00 - 17:00</Box>
-            <Flex>
-              <Box>Clock-in</Box>
-              <Box>Clock-out</Box>
+            <Flex
+              justifyContent={"space-around"}
+              padding={"5px"}
+              fontWeight={"bold"}
+            >
+              <Box
+                _hover={{
+                  bgColor: "#394867",
+                  cursor: "pointer",
+                  color: "#F1F6F9",
+                }}
+                bgColor={"#F1F6F9"}
+                color={"#212A3E"}
+                borderRadius={"5px"}
+                padding={"5px"}
+                onClick={cin}
+              >
+                Clock-in
+              </Box>
+              <Box
+                _hover={{
+                  bgColor: "#394867",
+                  cursor: "pointer",
+                  color: "#F1F6F9",
+                }}
+                bgColor={"#F1F6F9"}
+                color={"#212A3E"}
+                borderRadius={"5px"}
+                padding={"5px"}
+                onClick={cout}
+              >
+                Clock-out
+              </Box>
             </Flex>
           </Box>
-        </Box>
+          <Box
+            bgColor={"white"}
+            boxShadow={
+              "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px"
+            }
+            minHeight={"80px"}
+            padding={"5px"}
+          >
+            {clockIn.map((time, index) => (
+              <Flex justifyContent={"space-around"} bgColor={"#9BA4B5"}>
+                <Box>
+                  <Moment format="llll" date={time} />
+                </Box>
+                <Box>clock-in</Box>
+              </Flex>
+            ))}
+
+            {clockOut.map((time, index) => (
+              <Flex justifyContent={"space-around"} bgColor={"#9BA4B5"}>
+                <Box>
+                  <Moment format="llll" date={time} />
+                </Box>
+                <Box>clock-Out</Box>
+              </Flex>
+            ))}
+            {/* <AttendanceLog /> */}
+          </Box>
+        </Flex>
         <BottomNavbar />
       </Container>
     </>
@@ -28,28 +148,38 @@ export default function DashboardPage() {
 
 function BottomNavbar() {
   return (
-    <Box h={"40px"} w={"100%"} bgColor={"#394867"} color={"#F1F6F9"}>
+    <Box
+      h={"40px"}
+      w={"100%"}
+      bgColor={"#394867"}
+      color={"#F1F6F9"}
+      boxShadow={
+        "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px"
+      }
+      bottom={"0"}
+      // position={"absolute"}
+    >
       <Flex justifyContent={"space-evenly"} alignContent={"center"} h={"100%"}>
         <Box
-          _hover={{ bgColor: "#212A3E" }}
-          borderRadius={"10px"}
+          _hover={{ bgColor: "#212A3E", cursor: "pointer" }}
           padding={"5px"}
+          w={"33.33%"}
         >
-          Home
+          <Center>Home</Center>
         </Box>
         <Box
-          _hover={{ bgColor: "#212A3E" }}
-          borderRadius={"10px"}
+          _hover={{ bgColor: "#212A3E", cursor: "pointer" }}
           padding={"5px"}
+          w={"33.33%"}
         >
-          History
+          <Center>History</Center>
         </Box>
         <Box
-          _hover={{ bgColor: "#212A3E" }}
-          borderRadius={"10px"}
+          _hover={{ bgColor: "#212A3E", cursor: "pointer" }}
           padding={"5px"}
+          w={"33.33%"}
         >
-          Login
+          <Center>Login</Center>
         </Box>
       </Flex>
     </Box>
